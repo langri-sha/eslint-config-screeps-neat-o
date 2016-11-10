fc2830711947764db61fc928e5ef81d048285084
Filip Dupanović <filip.dupanovic@gmail.com>
2016-11-10 15:12:40 +0100

    style(test): Remove log output

--- test.js
+++ test.js
@@ -3,7 +3,6 @@ import test from 'ava'
 import pkg from './package'
 
 test('Test API Reference URL in build', t => {
-  console.log('Foo', pkg.scripts.build)
   t.truthy(pkg.scripts.build.match(
     /http:\/\/support\.screeps\.com\/hc\/en-us\/articles\/\d+-API-Reference/
   ))
